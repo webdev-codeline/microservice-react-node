41b715eb2a93ade5bf79f5684da16f7c71fa59a0
webdev-codeline <rain116093@hotmail.com>
2023-09-25 10:11:26 -0700

    create CreateComment component

---
+++ blog-boilerplate/client/src/CommentCreate.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+export const CommentCreate = ({ postId }) => {
+    const [content, setContent] = useState('');
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        });
+        setContent('');
+    }
+    return <div>
+            <form onSubmit={onSubmit}>
+                <div className='form-group'>
+                    <label>New Comment</label>
+                    <input 
+                    value={content} 
+                    className='form-control'
+                    onChange={e => setContent(e.target.value)}
+                    ></input>
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>;
+}
