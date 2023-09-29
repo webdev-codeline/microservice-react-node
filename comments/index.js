d79b8aa9a3dc1cda2ff8c6026e74d3260ce8baa8
webdev-codeline <rain116093@hotmail.com>
2023-09-29 09:32:53 -0700

    add axios and emitter to the services

--- comments/index.js
+++ comments/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
-
+const axios = require('axios');
 const app = express();
 
 const commentsByPostId = {};
@@ -12,13 +12,21 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []) ;
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({id: commentId, content});
-    console.log(comments);
+
     commentsByPostId[req.params.id] = comments;
+    await axios.post('http://localhost:4005/events', {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
     res.status(201).send(commentsByPostId);
 });
 
