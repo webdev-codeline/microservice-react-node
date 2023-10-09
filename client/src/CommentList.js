6171e4b3220c5ee3fd355bbe5b42e382847160f5
webdev-codeline <rain116093@hotmail.com>
2023-10-09 12:15:38 -0700

    update the client to use query service for fetching

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export const CommentList = ({ postId }) => {
+export const CommentList = ({ comments }) => {
 
-    const [comments, setComments] = useState([]);
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-        setComments(res.data);
-    }
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const renderedComments = comments.map(comment => {
+      const renderedComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
     })
     return <ul>
